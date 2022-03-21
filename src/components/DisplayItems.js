74c736a798ecf2b1de43f58e43ea1f2ed687c99e
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-03-21 17:33:13 -0400

    added images to displayItems

--- src/components/DisplayItems.js
+++ src/components/DisplayItems.js
@@ -4,7 +4,8 @@ export default function DisplayItems(props) {
   const cryJSX = Crystals.map((crystal) => {
     //maps over crystal array to make JSX with crystal info
     return (
-      <div key={crystal.name}>
+      <div className="display-card" key={crystal.name}>
+        <img className="cart-picture" src={crystal.src} alt={crystal.name} />
         <h1>{crystal.name}</h1>
         <h2>${crystal.price}</h2>
         <h3>{crystal.rarity}</h3>
--- src/components/crystals.js
+++ src/components/crystals.js
@@ -1,30 +1,41 @@
 const Crystals = [
   {
+    src:
+      "https://www.ajsgem.com/sites/default/files/ajs/natural-taaffeite-00002.jpg",
     name: "Taafeite",
     price: "35,000",
     rarity: "extremely rare"
   },
   {
+    src:
+      "https://media.istockphoto.com/photos/ruby-gemstone-picture-id177077514?k=20&m=177077514&s=612x612&w=0&h=u16y0TpivHSIS5NVv2KRq9sx8v_R0RgurDVHwyySfrU=",
     name: "Red Beryl",
     price: "5,000",
     rarity: "very rare"
   },
   {
+    src:
+      "https://media.istockphoto.com/photos/dark-purple-amethyst-picture-id497304679?b=1&k=20&m=497304679&s=170667a&w=0&h=GApiwmT9oRPME6pIAMcpFTXJ8NQGYiiMRM7_DA7cxok=",
     name: "Amethyst",
     price: "20",
     rarity: "common"
   },
   {
+    src:
+      "https://www.healingwithcrystals.net.au/uploads/1/5/5/7/15572098/green-malachite_1_orig.jpg",
     name: "Malachite",
     price: "50",
     rarity: "common"
   },
   {
+    src: "https://www.gia.edu/images/polished-jade-gem.png",
     name: "Jade",
     price: "500",
     rarity: "rare"
   },
   {
+    src:
+      "https://media.istockphoto.com/photos/closeup-of-a-sparkling-red-ruby-picture-id172373115?k=20&m=172373115&s=612x612&w=0&h=Q9fXXbmdgwD6YfBODeLKRNF4Ig0pu3UX1mVRbQfYk6g=",
     name: "Ruby",
     price: "800",
     rarity: "rare"
